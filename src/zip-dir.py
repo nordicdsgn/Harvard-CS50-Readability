b1c049b4cfd63dbbc9974db3c2c00ae2d92d70e6
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-02 06:15:06 +1000

    ignore

--- src/zip-dir.py
+++
@@ -1,9 +0,0 @@
-from shutil import make_archive as archive
-
-def main():
-    # zip this directory
-    fn = 'Harvard-CS-Readability'
-    archive(fn, 'zip', fn) # '../' + 
-
-if __name__ == '__main__':
-    main()
